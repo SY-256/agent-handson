940fa8ebd623ac55f347db8d81b606ab102df645
Shohei.Yamamoto <76563286+SY-256@users.noreply.github.com>
2025-11-06 13:38:24 +0000

    commit 11/6

--- chapter4/4_streamlit_app.py
+++ chapter4/4_streamlit_app.py
@@ -26,3 +26,35 @@ def reset_session():
 
 # セッション状態の初期化
 init_session_state()
+
+def run_agent(input_data):
+    """エージェントを実行し、結果を処理する"""
+    # AIエージェント呼び出しに使うconfigurationの作成
+    config = {"configurable":
+              {"thread_id": st.session_state.thread_id}
+    }
+
+    # 結果を処理
+    with st.spinner("処理中...", show_time=True):
+        for chunk in agent.stream(input_data, stream_mode="updates", config=config):
+            for task_name, result in chunk.items():
+                # interruptの場合
+                if task_name == "__interrupt__":
+                    st.session_state.tool_info = result[0].value
+                    st.session_state.waiting_approval = True
+                # 最終回答の場合
+                elif task_name == "agent":
+                    st.session_state.final_result = result.content
+                # LLM推論の場合
+                elif task_name == "invoke_llm":
+                    if isinstance(chunk["invoke_llm"].content, list):
+                        for content in result.content:
+                            if content["type"] == "text":
+                                st.session_state.messages.append({"role": "assistant", "content": content["text"]})
+                # ツール実行の場合
+                elif task_name == "use_tool":
+                    st.session_state.messages.append({"role": "assistant", "content": "ツール実行"})
+
+        st.rerun()
+
+
